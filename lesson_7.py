a28678282ca06fc31b0bd9a5fab2e122e2bf78b7
Lilit <lilit.khalapyan@gmail.com>
2020-09-17 19:49:47 +0400

    new list of dicts

--- lesson_7.py
+++ lesson_7.py
@@ -1,10 +1,26 @@
 pythonist_skills = [
-    {"Gohar" : "Python Basics"},
-    {"Lilit" : "OOP"},
-    {"Janna" : "Git"},
-    {"Luiza" : "C++"},
-    {"Lilit" : "Business Analyst"},
-    {"Janna" : "Numpy"},
-    {"Luiza" : "Animation"},
-    {"Gohar" : "Business Consultant"}
+    {"Gohar": "Python Basics"},
+    {"Lilit": "OOP"},
+    {"Janna": "Git"},
+    {"Luiza": "C++"},
+    {"Lilit": "Business Analyst"},
+    {"Janna": "Numpy"},
+    {"Luiza": "Animation"},
+    {"Gohar": "Business Consultant"}
 ]
+
+grand_list = []
+
+for dicts in pythonist_skills:
+    keyss = tuple(dicts.keys())
+    skills = list(dicts.values())[0]
+    skills_list = []
+    new_dicts = {keyss[0]: skills_list}
+    grand_list.append(new_dicts)
+    for dictss in grand_list:
+        new_key = tuple(dictss.keys())[0]
+        if keyss[0] == new_key:
+            print('lalala')
+            dictss[new_key].append(skills)
+
+print(grand_list)
